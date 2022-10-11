a758b1db32d514f1c2e7aa0a3d9348af8ab9d430
vladbuk <vladbuk@gmail.com>
2022-10-11 12:17:56 +0300

    arp

--- 01_linux-bash/linux-bash-1.sh
+++ 01_linux-bash/linux-bash-1.sh
@@ -1,33 +1,51 @@
 #!/bin/bash
 
+arpGrep () {
+  echo "$(arp $1 | tail -n 1 | awk '{print $1}')"
+}
 
 showSubnetIP () {
- echo "$(sudo nmap -sP -n $(ip -o address | awk '/scope global/ {print $4}' | head -n 1) | awk '/Nmap scan/ {print $5}')"
+  if [[ `/usr/bin/which nmap` ]]
+  then 
+    subnetip="$(sudo nmap -sP -n $(ip -o address | awk '/scope global/ {print $4}' | \
+     head -n 1) | awk '/Nmap scan/ {print $5}')"
+    for ip in $subnetip
+    do
+      echo "$ip [$(arpGrep $ip)]"
+    done
+  else
+    echo "To run this script you have to install \"nmap\""
+  fi
 }
 
 showPorts () {
- echo "$(sudo netstat -tulpn | grep LISTEN | awk '{print $1,$4}')"
+  if [[ `/usr/bin/which netstat` ]]
+  then
+    echo "$(sudo netstat -tulpn | grep LISTEN | awk '{print $1,$4}')"
+  else
+    echo "To run this script you have to install \"netstat\""
+  fi
 }
 
 
 main () {
 
-MESSAGE="You have to use one parameter:\n
+  MESSAGE="You have to use one parameter:\n
 --all - to display IP addresses and symbolic names of all hosts in the current subnet\n
 --target  - to display a list of open system TCP ports"
 
-if [[ $# -ne 1 ]]
-then
-  echo -e $MESSAGE
-elif [[ $1 == "--all" ]]
-then
-  showSubnetIP
-elif [[ $1 == "--target" ]]
-then
-  showPorts
-else
-  echo -e $MESSAGE
-fi
+  if [[ $# -ne 1 ]]
+  then
+    echo -e $MESSAGE
+  elif [[ $1 == "--all" ]]
+  then
+    showSubnetIP
+  elif [[ $1 == "--target" ]]
+  then
+    showPorts
+  else
+    echo -e $MESSAGE
+  fi
 
 }
 
