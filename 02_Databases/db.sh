d3452fee522fae9ed5884ecb97a950327b9a2f01
vladbuk <vladbuk@gmail.com>
2022-10-17 19:00:06 +0300

    few sql queris

--- 02_Databases/db.sh
+++ 02_Databases/db.sh
@@ -14,3 +14,8 @@ mysql -u root -p -e 'show databases;'
 
 # import database from host mysql client
 mysq -u root -p --protocol=tcp < db.sql
+
+# MySQL queries
+select count(*) from orders;
+select productCode,quantity from orders where customerId = 2;
+select name, email from customers;
