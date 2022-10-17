ae3530623b5e93e0491042a8d44ebeb057f46aab
vladbuk <vladbuk@gmail.com>
2022-10-17 15:39:26 +0300

    values

--- 02_Databases/db.sql
+++ 02_Databases/db.sql
@@ -5,20 +5,35 @@ DROP TABLE if EXISTS products;
 create table products (
     id INT(8) not null auto_increment,
     code varchar(12) not null,
-    title varchar(48) not null,
+    category_id INT(8) not null,
+    title varchar(128) not null,
     description varchar(1024) not null,
     price decimal(6,2) not null,
     quantity int(5) not null,
     primary key (id, code)
 );
 
+insert  into products (code, title, description, price, quantity) values 
+(1001,'Learn Python: The Complete Python Programming Course','Learn A-Z everything about Python, from the basics, to advanced topics like Python GUI, Python Data Analysis, and more!',19.99,100),
+(1002,'The Complete 2022 Web Development Bootcamp','Become a Full-Stack Web Developer with just ONE course. HTML, CSS, Javascript, Node, React, MongoDB, Web3 and DApps',19.99,100),
+(1003,'The English Master Course','The Complete English Language Course: English grammar, English speaking, and writing. British and American English.',9.99,50),
+(1004,'Spanish for Beginners','Learn Spanish with the complete, non-stop SPEAKING method, in a matter of weeks, not years.',9.99,50),
+(1005,'15 Minutes x 15 Days Yoga','Improve your flexibility, reduce morning stiffness and alleviate recurring aches and pains in 15 minutes a day.',29.99,10),
+(1006,'Breath is Life: Pranayama, meditation course - Yoga Alliance','Pranayama / Meditation / Bandhas course that will change your life. (With 16 hours of Yoga Alliance Continuing Points)',29.99,10);
+
+
 DROP TABLE if EXISTS categories;
-create table categoires (
+create table categories (
     id int(8) not null auto_increment,
     title varchar(48) not null,
     description varchar(1024) not null,
     primary key (id)
 );
+
+insert into categories (title, description) values 
+('Software courses','Courses for people who want to become developers'),
+('Language courses','Different foreign language courses'),
+('Yoga courses','Big collection of the yoga video courses');
 
 DROP TABLE if EXISTS customers;
 
@@ -26,17 +41,36 @@ CREATE TABLE customers (
     id INT(8) NOT NULL auto_increment,
     name VARCHAR(48),
     email VARCHAR(48),
-    phone VARCHAR(12),
-    address VARCHAR(50),
+    phone VARCHAR(16),
+    address VARCHAR(250),
     PRIMARY KEY (id)
 );
+
+insert into customers (name, email, phone, address) values 
+('Hope Macejkovic','lynch.ramon@gmail.com','(302) 871-9295','33844 Maymie Dam Apt. 098, Vonborough, West Virginia'),
+('Vernice Wisoky','schaden.murl@langworth.com','906-717-2201','45608 Craig Junction Suite 776, New Triston'),
+('Rick Greenholt','elroy96@bahringer.net','906-650-9426','8895 Jamarcus River Apt. 543'),
+('Daija Ortiz','ardith54@yahoo.com','965.340.6943','74815 Effertz Springs Apt. 275'),
+('Eriberto Wunsch','hfriesen@waters.org','1-617-229-5927','78129 Lesch Spur Apt. 990');
 
 DROP TABLE IF EXISTS orders;
 
 CREATE TABLE orders (
-  id int(11) NOT NULL,
+  id int(11) NOT NULL auto_increment,
+  customerId int(8) NOT NULL,
   productCode varchar(15) NOT NULL,
   quantity int(11) NOT NULL,
-  price decimal(10,2) NOT NULL,
   PRIMARY KEY (id)
 );
+
+insert into orders (customerId, productCode, quantity) values 
+(1, 1001, 1),
+(1, 1002, 1),
+(2, 1004, 1),
+(2, 1005, 1),
+(3, 1006, 1),
+(4, 1003, 1),
+(4, 1004, 1),
+(4, 1007, 1),
+(5, 1002, 1),
+(5, 1006, 1);
