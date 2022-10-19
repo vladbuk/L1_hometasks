e93b00a4abf02ae59b6e3d10cdb7a76d2136bb78
vladbuk <vladbuk@gmail.com>
2022-10-19 14:47:04 +0300

    functions added

--- 01_linux-bash/linux-bash-2.sh
+++ 01_linux-bash/linux-bash-2.sh
@@ -1,18 +1,44 @@
 #!/bin/bash
 
-# 1. Top 5 IP from which were the most requests
-cat example_log.log | awk '{print $1}' | sort | uniq -c | sort -nr | awk '{print "From IP " $2 "\t were " $1 " requests" }' | head -n 5
+param=$1
 
-# 2. Top 5 the most requested pages
-cat example_log.log | cut -d\" -f2 | awk '{print $2}' | egrep -iv "ico$|woff|ttf" | sort | uniq -c | sort -nr | head -n 5
+top5ip () {
+    echo "1. Top 5 IP from which were the most requests"
+    local result=$(cat $param | awk '{print $1}' | sort | uniq -c | sort -nr | awk '{print "From IP " $2 "\t were " $1 " requests" }' | head -n 5)
+    echo -e "$result\n"
+}
 
-# 3. How many requests were there from each ip?
-cat example_log.log | awk '{print $1}' | sort | uniq -c | sort -nr | awk '{ip = $2; r = $1; printf "From IP %-15s - %d request%s\n", ip, r, r == 1 ? "" : "s"}'
+top5pages () {
+    echo "2. Top 5 the most requested pages"
+    result=$(cat $param | cut -d\" -f2 | awk '{print $2}' | egrep -iv "ico$|woff|ttf" | sort | uniq -c | sort -nr | head -n 5 | sed -e 's/^[ \t]*//')
+    echo -e "$result\n"
+}
 
-# 4. What non-existent pages were clients reffered to?
-cat example_log.log | grep " 404 " | cut -d\" -f2 | awk '{print $2}' | sort | uniq
+requestIpCount () {
+    # 3. How many requests were there from each ip?
+    cat $1 | awk '{print $1}' | sort | uniq -c | sort -nr | awk '{ip = $2; r = $1; printf "From IP %-15s - %d request%s\n", ip, r, r == 1 ? "" : "s"}'
+}
 
-# 5. What time did site get the most requests?
+nonExistPages () {
+    # 4. What non-existent pages were clients reffered to?
+    cat $1 | grep " 404 " | cut -d\" -f2 | awk '{print $2}' | sort | uniq
+}
 
-# 6. What search bots have accessed the site (UA + IP)?
+mostTimeRequest () {
+    # 5. What time did site get the most requests?
+    echo
+}
 
+showSearchBot () {
+    # 6. What search bots have accessed the site (UA + IP)?
+    echo
+}
+
+if [[ $# -ne 1 ]]
+then
+    echo -e "For running script it needs one argument - \nlog file name with full path"
+else
+    #cat $1
+    top5ip
+    top5pages
+fi
