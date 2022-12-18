25188e5f53bfc54b8734b0e7829b74785622b5d9
vladbuk <vladbuk@gmail.com>
2022-12-18 17:52:06 +0200

    Create a data backup script

---
+++ 01_linux-bash/linux-bash-3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Create a data backup script
+# Add this to crontab: * * * * * /path/to/this-script
+#
+
+if [[ $# -eq 2 ]]
+then
+    src=$1
+    dest=$2
+    rsync -avz --progress --delete $src $dest --log-file=/tmp/folder_backup.log
+    echo -e "\n"
+    echo "Logs writes to /tmp/folder_backup.log"
+else
+    echo "This script need 2 arguments: source dir and destination dir."
+fi
