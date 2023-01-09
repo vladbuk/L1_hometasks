7d896b36918bd84be751bf50616edb703b5d9b08
vladbuk <vladbuk@gmail.com>
2023-01-09 20:40:08 +0200

    route53

---
+++ _terraform/05-ubuntu-2-env/route53.tf
@@ -0,0 +1,21 @@
+data "aws_route53_zone" "selected" {
+  name         = "vladbuk.site."
+  private_zone = false
+}
+
+resource "aws_route53_record" "test" {
+  zone_id = data.aws_route53_zone.selected.zone_id
+  name    = "test.${data.aws_route53_zone.selected.name}"
+  type    = "A"
+  ttl     = 3600
+  records = [aws_instance.t2micro_ubuntu_test.public_ip]
+}
+
+resource "aws_route53_record" "prod" {
+  zone_id = data.aws_route53_zone.selected.zone_id
+  name    = "prod.${data.aws_route53_zone.selected.name}"
+  type    = "A"
+  ttl     = 3600
+  records = [aws_instance.t2micro_ubuntu_prod.public_ip]
+}
+
